d003251c377e40a3d39452c0c98d700f0489ad6d
Asif047 <asifulhaque047@gmail.com>
2017-07-31 19:51:17 +0600

    first commit

---
+++ Book_share_borrow/src/book_share_borrow/User2.java
@@ -0,0 +1,49 @@
+
+
+// this part of work is done by ID : 13.01.04.047
+
+package book_share_borrow;
+
+
+public class User2 {
+    
+    private int BookId;
+    private String BookName;
+    private String BookISBN;
+    private String Author;
+    private int AvailableCopy;
+    
+    public User2(int BookId, String BookName, String BookISBN,String Author, int AvailableCopy)
+    {
+        this.BookId = BookId;
+        this.BookName = BookName;
+        this.BookISBN = BookISBN;
+        this.Author = Author;
+        this.AvailableCopy = AvailableCopy;
+    }
+    
+    public int getBookId()
+    {
+        return BookId;
+    }
+    
+    public String getBookName()
+    {
+        return BookName;
+    }
+    
+    public String getBookISBN()
+    {
+        return BookISBN;
+    }
+    
+    public String getAuthor()
+    {
+        return Author;
+    }
+    
+    public int getAvailableCopy()
+    {
+        return AvailableCopy;
+    }
+}
